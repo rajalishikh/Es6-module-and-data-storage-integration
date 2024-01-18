06aaf3d4c3c3749dc1a3e2f1262131cb816ca7ba
raj ali sheikhe <nahidujjamanraj@gmail.comm>
2024-01-18 09:34:57 +0600

    add my export import part

---
+++ src/Component/Sunglass/Sunglass.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Watch from '../Watch/Watch';
+import add from '../utility/Calculation';
+import './Sunglass.css';
+const first = 1111;
+const second = 222;
+const sum = add(first + second);
+
+
+
+const Sunglass = () => {
+    return (
+        <div>
+            <Watch></Watch>
+            
+            
+            
+        </div>
+    );
+};
+
+export default Sunglass;
